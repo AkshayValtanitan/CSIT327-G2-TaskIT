e9d6acf4a7b4a7c911a90d5cd6149be54993bef0
AkshayValtanitan <shanenathanarchival@gmail.com>
2026-02-25 16:08:05 +0800

    Configure DATABASE_URL for Supabase and update settings

--- taskit_project/settings.py
+++ taskit_project/settings.py
@@ -147,20 +147,20 @@ WSGI_APPLICATION = 'taskit_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 # Default to SQLite for local development
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-# DATABASE_URL = os.environ.get("DATABASE_URL")
 # DATABASES = {
-#     "default": dj_database_url.config(
-#         default=DATABASE_URL,
-#         conn_max_age=600,
-#         ssl_require=True
-#     )
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
 # }
+DATABASE_URL = os.environ.get("DATABASE_URL")
+DATABASES = {
+    "default": dj_database_url.config(
+        default=DATABASE_URL,
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
 # postgresql://postgres.bwaczilydwpkqlrxdjoq:N0V11Fr1092Oo9@aws-1-us-east-2.pooler.supabase.com:5432/postgres
 
 
